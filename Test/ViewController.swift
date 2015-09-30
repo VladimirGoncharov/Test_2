beb63e82429b783f9a5d7380e48cb08d8a33e245
VladimirGoncharov <vladimir1631@yandex.ru>
2015-09-30 16:18:58 +0400

    add viewWillAppear:

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
